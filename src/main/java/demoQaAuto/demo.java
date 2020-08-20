f9109ab43f579ea0f358199b488801c33d4bfd23
svkangane <sanjay.kangane1@gmail.com>
2020-08-21 00:23:29 +0530

    MavenTemp

---
+++ src/main/java/demoQaAuto/demo.java
@@ -0,0 +1,7 @@
+package demoQaAuto;
+
+public class demo {
+public static void main(String[] args) {
+	System.out.println("Src main java");
+}
+}
---
+++ src/test/java/demoQaAuto/newdemo.java
@@ -0,0 +1,7 @@
+package demoQaAuto;
+
+public class newdemo {
+	public static void main(String[] args) {
+		System.out.println("Src test java");
+	}
+}
